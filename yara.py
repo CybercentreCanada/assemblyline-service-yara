effad52076b3fd5485b1a94159e0912d31a28cae
sgaron-cse <sgaron.cse@gmail.com>
2017-02-27 16:10:13 -0500

    Paranoid rule check raise yara.SyntaxError for clearer error message

--- yara.py
+++ yara.py
@@ -401,7 +401,7 @@ class Yara(ServiceBase):
 
         if stderr or stdout != 'It worked!\n':
             if "yara.SyntaxError" in stderr:
-                raise yara.SyntaxError(stderr.split("yara.SyntaxError: "))
+                raise yara.SyntaxError(stderr.split("yara.SyntaxError: ")[1])
             else:
                 raise Exception("Paranoid rule check failed! " + stderr)
 

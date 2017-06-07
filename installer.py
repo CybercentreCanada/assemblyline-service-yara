ed9869223d6678ba065f8f7caa40af314ef2f651
ekkerri <ekkerri.cse@gmail.com>
2017-06-07 14:50:01 -0400

    since other services install yara, no need to repeat code.

--- installer.py
+++ installer.py
@@ -4,19 +4,23 @@ import os
 
 def install(alsi):
 
-    local_yara_support = os.path.join(alsi.alroot, 'support/yara/')
+    # Config Decoder service also installs Yara, putting here:
+    alsi.install_yara_3()
 
+    # If you need to use libyara outside of yara-python, you need to build Yara separately as shown (TODO: Test):
+    # local_yara_support = os.path.join(alsi.alroot, 'support/yara/')
+    #
     # Build commands
-    #local_yara = os.path.join(local_yara_support, 'yara-3.6.1.tar.gz')
-    #alsi.fetch_package('yara-3.6.1.tar.gz', local_yara)
-
+    # local_yara = os.path.join(local_yara_support, 'yara-3.6.1.tar.gz')
+    # alsi.fetch_package('yara-3.6.1.tar.gz', local_yara)
+    #
     # alsi.sudo_apt_install([
     #     'automake',
     #     'libtool',
     #     'make',
     #     'gcc',
     # ])
-
+    #
     # alsi.runcmd("cd {0}".format(local_yara_support))
     # alsi.runcmd("tar -zxf yara-3.6.1.tar.gz")
     # alsi.runcmd("cd yara-3.6.1")
@@ -24,20 +28,15 @@ def install(alsi):
     # alsi.runcmd("./configure --enable-dotnet")
     # alsi.runcmd("make")
     # alsi.runcmd("sudo make install")
-
-    # Running install and statically binding libyara to yara-python. Only use dynamic-linking option if you need
-    # to use libyara outside of yara-python. If you need this option, you need to build Yara standalone as shown above,
-    # and run this below instead:
+    #
+    # local_yara_python = os.path.join(local_yara_support, 'yara-python-3.6.1.tar.gz')
+    # alsi.fetch_package('yara-python-3.6.1.tar.gz', local_yara_python)
+    #
+    # alsi.runcmd("cd {0}" .format(local_yara_support))
+    # alsi.runcmd("tar -zxf yara-python-3.6.1.tar.gz")
+    # alsi.runcmd("cd yara-python-3.6.1")
+    # alsi.runcmd("python setup.py build --enable-dotnet")
     # alsi.runcmd("sudo python setup.py install --dynamic-linking")
-
-    local_yara_python = os.path.join(local_yara_support, 'yara-python-3.6.1.tar.gz')
-    alsi.fetch_package('yara-python-3.6.1.tar.gz', local_yara_python)
-
-    alsi.runcmd("cd {0}" .format(local_yara_support))
-    alsi.runcmd("tar -zxf yara-python-3.6.1.tar.gz")
-    alsi.runcmd("cd yara-python-3.6.1")
-    alsi.runcmd("python setup.py build --enable-dotnet")
-    alsi.runcmd("sudo python setup.py install")
 
 if __name__ == '__main__':
     from assemblyline.al.install import SiteInstaller

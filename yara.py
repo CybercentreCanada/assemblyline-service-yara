ff05c27bc5b56d224ea29a8296c5ae1f58d1f4d9
Erik <erik.holmes@cst-cse.gc.ca>
2019-08-14 15:12:00 -0400

    created threat_actor tags for new CCCS YARA Format

--- yara.py
+++ yara.py
@@ -47,10 +47,17 @@ class YaraMetadata(object):
         self.weight = meta.get('weight', 0)  # legacy rule format
         self.al_status = meta.get('al_status', "DEPLOYED")
 
+        self.actor_type = meta.get('actor_type', None)
+        if not self.actor_type:
+            self.actor_type = meta.get('ta_type', None)
+
         def _safe_split(comma_sep_list):
             return [e for e in comma_sep_list.split(',') if e]
 
         self.actors = _safe_split(match.meta.get('used_by', ''))
+        if not self.actors:
+            self.actors = _safe_split(match.meta.get('actor', ''))
+
         self.summary = _safe_split(match.meta.get('summary', ''))
         self.exploits = _safe_split(match.meta.get('exploit', ''))
 
@@ -189,6 +196,10 @@ class Yara(ServiceBase):
         result.append_tag(Tag(TAG_TYPE.FILE_YARA_RULE, match.rule, TAG_SCORE.SURE, classification=almeta.classification,
                               usage=TAG_USAGE.IDENTIFICATION))
         title_elements = [match.rule, ]
+
+        if almeta.actor_type:
+            result.append_tag(Tag(TAG_TYPE.THREAT_ACTOR, almeta.actor_type, TAG_SCORE.NULL,
+                                  classification=almeta.classification, usage=TAG_USAGE.IDENTIFICATION))
 
         # Implant Tags.
         implant_title_elements = []

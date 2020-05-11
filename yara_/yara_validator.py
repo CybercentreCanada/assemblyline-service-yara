8a9c908ac6ce5213fc7aff89af1dbc02b259051c
Steve Garon <steve.garon@cyber.gc.ca>
2020-05-11 09:49:32 -0400

    Fix file cleaner to handle if the last rules is wrong and to be able to rename a rule

--- yara_/yara_validator.py
+++ yara_/yara_validator.py
@@ -23,49 +23,55 @@ class YaraValidator(object):
         self.rulestart = re.compile(r'^(?:global )?(?:private )?(?:private )?rule ', re.MULTILINE)
         self.rulename = re.compile('rule ([^{^:]+)')
 
-    def clean(self, rulefile, eline, message):
-
+    def clean(self, rulefile, eline, message, invalid_rule_name):
         with open(rulefile, 'r') as f:
             f_lines = f.readlines()
         # List will start at 0 not 1
         error_line = eline - 1
 
-        # First loop to find start of rule
-        start_idx = 0
-        while True:
-            find_start = error_line - start_idx
-            if find_start == -1:
-                raise Exception("Yara Validator failed to find invalid rule start. "
-                                f"Yara Error: {message} Line: {eline}")
-            line = f_lines[find_start]
-            if re.match(self.rulestart, line):
-                rule_error_line = (error_line - find_start)
-                invalid_rule_name = re.search(self.rulename, line).group(1).strip()
+        if invalid_rule_name:
+            f_lines[error_line] = f_lines[error_line].replace(invalid_rule_name, f"{invalid_rule_name}_1")
+            self.log.warning(f"Yara rule '{invalid_rule_name}' was renamed '{invalid_rule_name}_1' because it's "
+                             f"rule name was used more then once.")
+        else:
+            # First loop to find start of rule
+            start_idx = 0
+            while True:
+                find_start = error_line - start_idx
+                if find_start == -1:
+                    raise Exception("Yara Validator failed to find invalid rule start. "
+                                    f"Yara Error: {message} Line: {eline}")
+                line = f_lines[find_start]
+                if re.match(self.rulestart, line):
+                    invalid_rule_name = re.search(self.rulename, line).group(1).strip()
 
-                # Second loop to find end of rule
-                end_idx = 1
-                while True:
-                    find_end = error_line + end_idx
-                    if find_end > len(f_lines):
-                        raise Exception("Yara Validator failed to find invalid rule end. "
-                                        f"Yara Error: {message} Line: {eline}")
-                    line = f_lines[find_end]
-                    if re.match(self.rulestart, line):
-                        # Now we have the start and end, strip from file
-                        rule_file_lines = f_lines[:find_start] + f_lines[find_end:]
+                    # Second loop to find end of rule
+                    end_idx = 0
+                    while True:
+                        find_end = error_line + end_idx
+                        if find_end >= len(f_lines):
+                            raise Exception("Yara Validator failed to find invalid rule end. "
+                                            f"Yara Error: {message} Line: {eline}")
+                        line = f_lines[find_end]
+                        if re.match(self.rulestart, line) or find_end == len(f_lines) - 1:
+                            # Now we have the start and end, strip from file
+                            if find_end == len(f_lines) - 1:
+                                f_lines = f_lines[:find_start]
+                            else:
+                                f_lines = f_lines[:find_start] + f_lines[find_end:]
+                            break
+                        end_idx += 1
+                    # Send the error output to AL logs
+                    error_message = f"Yara rule '{invalid_rule_name}' removed from rules file because of an error " \
+                                    f"at line {eline} [{message}]."
+                    self.log.warning(error_message)
+                    break
+                start_idx += 1
 
-                        with open(rulefile, 'w') as f:
-                            f.writelines(rule_file_lines)
-                        break
-                    end_idx += 1
-                # Send the error output to AL logs
-                error_message = f"Yara rule '{invalid_rule_name}' removed from rules file because of an error " \
-                                f"at line {rule_error_line} [{message}]."
-                self.log.warning(error_message)
-                break
-            start_idx += 1
+        with open(rulefile, 'w') as f:
+            f.writelines(f_lines)
 
-        return invalid_rule_name, rule_error_line
+        return invalid_rule_name
 
     def paranoid_rule_check(self, rulefile):
         # Run rules separately on command line to ensure there are no errors
@@ -101,13 +107,18 @@ class YaraValidator(object):
 
             # If something goes wrong, clean rules until valid file given
             except Exception as e:
+                error = str(e)
                 change = True
-                if str(e).startswith('yara.SyntaxError'):
+                if error.startswith('yara.SyntaxError'):
 
-                    e_line = int(str(e).split('):', 1)[0].split("(", -1)[1])
-                    e_message = str(e).split("): ", 1)[1]
+                    e_line = int(error.split('):', 1)[0].split("(", -1)[1])
+                    e_message = error.split("): ", 1)[1]
+                    if "duplicated identifier" in error:
+                        invalid_rule_name = e_message.split('"')[1]
+                    else:
+                        invalid_rule_name = ""
                     try:
-                        self.clean(rulefile, e_line, e_message)
+                        self.clean(rulefile, e_line, e_message, invalid_rule_name)
                     except Exception as ve:
                         raise ve
 

3d137f3db63edf35321274160fdc93fd4db0c831
cccs-rs <ryan.samaroo@cyber.gc.ca>
2024-01-02 17:03:31 +0000

    Add "r" to STRING_ESCAPE_CHARS in plyara

--- yara_/update_server.py
+++ yara_/update_server.py
@@ -10,11 +10,12 @@ from assemblyline.common import forge
 from assemblyline_client import get_client
 from assemblyline_v4_service.updater.updater import UI_SERVER, UPDATER_API_ROLES, ServiceUpdater, temporary_api_key
 from plyara import Plyara, utils
+
 from yara_.helper import YARA_EXTERNALS, YaraImporter, YaraValidator
 
 classification = forge.get_classification()
 
-UPDATER_API_ROLES.append('signature_manage')
+UPDATER_API_ROLES.append("signature_manage")
 
 
 def _compile_rules(rules_file, externals, logger: logging.Logger):
@@ -42,7 +43,7 @@ def guess_category(rule_file_name: str) -> Optional[str]:
         "info": ["info", "deprecated", "crypto", "packer"],
         "tool": ["webshell"],
         "exploit": ["cve", "exploit"],
-        "malware": ["malware", "maldoc", "implant"]
+        "malware": ["malware", "maldoc", "implant"],
     }
 
     for cat, items in cat_map.items():
@@ -60,10 +61,10 @@ def replace_include(include, dirname, processed_files: set[str], cur_logger: log
         cur_logger.info(f"File doesn't exist: {full_include_path}")
         return [], processed_files
 
-    temp_lines = ['\n']  # Start with a new line to separate rules
+    temp_lines = ["\n"]  # Start with a new line to separate rules
     if full_include_path not in processed_files:
         processed_files.add(full_include_path)
-        with open(full_include_path, 'r') as include_f:
+        with open(full_include_path, "r") as include_f:
             lines = include_f.readlines()
 
         for i, line in enumerate(lines):
@@ -122,7 +123,7 @@ class YaraUpdateServer(ServiceUpdater):
     def import_update(self, files_sha256, client, source_name: str, default_classification=classification.UNRESTRICTED):
         processed_files: set[str] = set()
 
-        with tempfile.NamedTemporaryFile(mode='a+', suffix=source_name) as compiled_file:
+        with tempfile.NamedTemporaryFile(mode="a+", suffix=source_name) as compiled_file:
             # Aggregate files into one major source file
             for file, _ in files_sha256:
                 # File has already been processed before, skip it to avoid duplication of rules
@@ -133,14 +134,13 @@ class YaraUpdateServer(ServiceUpdater):
 
                 file_dirname = os.path.dirname(file)
                 processed_files.add(os.path.normpath(file))
-                with open(file, 'r', errors="surrogateescape") as f:
+                with open(file, "r", errors="surrogateescape") as f:
                     f_lines = f.readlines()
 
                 temp_lines: list[str] = []
                 for _, f_line in enumerate(f_lines):
                     if f_line.startswith("include"):
-                        lines, processed_files = replace_include(
-                            f_line, file_dirname, processed_files, self.log)
+                        lines, processed_files = replace_include(f_line, file_dirname, processed_files, self.log)
                         temp_lines.extend(lines)
                     else:
                         temp_lines.append(f_line)
@@ -148,6 +148,7 @@ class YaraUpdateServer(ServiceUpdater):
                 # guess the type of files that we have in the current file
                 guessed_category = guess_category(file)
                 parser = Plyara()
+                parser.STRING_ESCAPE_CHARS.add("r")
                 # Try parsing the ruleset; on fail, move onto next set
                 try:
                     signatures: list[dict[str, Any]] = parser.parse_string("\n".join(temp_lines))
@@ -159,21 +160,21 @@ class YaraUpdateServer(ServiceUpdater):
                     # Guess category
                     if guessed_category:
                         for s in signatures:
-                            s.setdefault('metadata', [])
+                            s.setdefault("metadata", [])
 
                             # Do not override category with guessed category if it already exists
-                            for meta in s['metadata']:
-                                if 'category' in meta:
+                            for meta in s["metadata"]:
+                                if "category" in meta:
                                     continue
 
-                            s['metadata'].append({'category': guessed_category})
-                            s['metadata'].append({guessed_category: s.get('rule_name')})
+                            s["metadata"].append({"category": guessed_category})
+                            s["metadata"].append({guessed_category: s.get("rule_name")})
 
                     # Save all rules from source into single file
                     for s in signatures:
                         # Fix imports and remove cuckoo
-                        s['imports'] = utils.detect_imports(s)
-                        if "cuckoo" not in s['imports']:
+                        s["imports"] = utils.detect_imports(s)
+                        if "cuckoo" not in s["imports"]:
                             compiled_file.write(utils.rebuild_yara_rule(s))
                 except Exception as e:
                     self.log.error(f"Problem parsing {file}: {e}")
@@ -182,12 +183,13 @@ class YaraUpdateServer(ServiceUpdater):
             try:
                 compiled_file.seek(0)
                 _compile_rules(compiled_file.name, self.externals, self.log)
-                yara_importer.import_file(compiled_file.name, source_name,
-                                          default_classification=default_classification)
+                yara_importer.import_file(
+                    compiled_file.name, source_name, default_classification=default_classification
+                )
             except Exception as e:
                 raise e
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with YaraUpdateServer(externals=YARA_EXTERNALS, default_pattern="*.yar*") as server:
         server.serve_forever()
